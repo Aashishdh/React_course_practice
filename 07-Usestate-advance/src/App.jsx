9cbb422aacccd970828f6f0e2e6ebf34097df72b
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-11 19:23:39 +0545

    Advance useState learned

---
+++ 07-Usestate-advance/src/App.jsx
@@ -0,0 +1,23 @@
+/*Badge update*/
+// import React, { useState } from 'react'
+// const App = () => {
+//   const [num, setnum] = useState(0)
+//   return (
+//     <div>
+//       <h1>{num}</h1>
+//       <button onClick={()=>{
+//         setnum(prev=>prev +1)
+//         setnum(prev=>prev +1)
+//         setnum(prev=>prev +1)
+//       }}>clicked</button>
+//     </div>
+//   )
+// export default App
+// }
+
+
+
+
+
+
+
