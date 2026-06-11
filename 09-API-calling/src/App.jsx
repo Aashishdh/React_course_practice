f5de5deb154d2e8502b0e73418357d0063be269b
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-11 19:25:17 +0545

    Learned both fetch and axios. Api calling

---
+++ 09-API-calling/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios'
+const App = () => {
+ /* async function getData(){
+    const response= await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    console.log(response);
+  }
+    */
+  //  Using Fetch method
+   /*const getData=async()=>{
+    const response= await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    const data=await response.json()
+    console.log(data);
+   }*/
+
+    // Using Axios method
+    /*const getData=async()=>{
+      const response=await axios.get('https://jsonplaceholder.typicode.com/users')
+
+      console.log(response);
+      
+    }
+      */
+
+    const getData=async()=>{
+      const response=await axios.get('https://jsonplaceholder.typicode.com/users')
+
+      console.log(response);
+      
+    }
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+    </div>
+  )
+}
+
+export default App
