c98b5c468e8a6db6302a0ed986d68268323571b0
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-11 19:21:25 +0545

    CSS review ... just few thing

---
+++ 04-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Components/Button'
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ 04-CSS/src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import'../index.css';
+import'../Styles/Button.css';
+const Button = () => {
+  return (
+    <div className='btn'>
+        Button
+    </div>
+  )
+}
+
+export default Button
