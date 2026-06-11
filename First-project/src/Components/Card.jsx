9ae56dc748415e80a498d3c54a459e1f79cf7509
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-11 19:27:13 +0545

    complete basic first project

---
+++ First-project/src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+                <h1>Aashish dhungana</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus natus, velit corrupti aut voluptatum fugit, voluptatem non quo deserunt, ratione maiores dolores neque minus voluptas animi culpa aliquam ipsa expedita.</p>
+              </div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ First-project/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+            This is navbar
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
